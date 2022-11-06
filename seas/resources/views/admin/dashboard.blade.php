b5f05684cc92e3a72435252245f0166cc7fd4897
alaminrifat <alaminrifat71@gmail.com>
2022-11-06 21:15:57 +0600

    first commit

---
+++ seas/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.main')
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+            <h4>Dashboard</h4>
+            <table class="table table-hover">
+                <thead>
+                    <th>Name</th>
+                    <th>Email</th>
+                </thead>
+                <tbody>
+                    <td>{{$LoggedUserInfo['name'] }}</td>
+                    <td>{{$LoggedUserInfo['email'] }}</td>
+                    <td><a href="{{ route('auth.logout')}}">Logout</a></td>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ seas/routes/web.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Middleware\AuthCheck;
+use App\Http\Middleware\TeacherCheck;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// ___________________________________________________________________________
+//                            Admin Route
+// ___________________________________________________________________________
+
+Route::post('/auth/save',[MainController::class, 'save'])->name('auth.save');
+Route::post('/auth/check',[MainController::class,'check'])->name('auth.check');
+Route::get('/auth/logout',[MainController::class,'logout'])->name('auth.logout');
+
+
+
+Route::group(['middleware'=>['AuthCheck']], function(){
+    Route::get('/auth/login',[MainController::class, 'login'])->name('auth.login');
+    Route::get('/auth/register',[MainController::class, 'register'])->name('auth.register');
+
+
+    Route::get('/admin/dashboard',[MainController::class,'dashboard'])->name('admin.dashboard');
+});
+
+// ___________________________________________________________________________
+//                            Teacher Route
+// ___________________________________________________________________________
+Route::get('/teacher/topics',[TeacherController::class,'topics'])->name('teacher.topics');
+Route::post('/teacher/topics',[TeacherController::class,'topicsSave'])->name('teacher.topicsSave');
+
+Route::get('/teacher/exams',[TeacherController::class,'exams'])->name('teacher.exams');
+Route::post('/teacher/exams',[TeacherController::class,'examsSave'])->name('teacher.examsSave');
+
+
+Route::get('/teacher/viewTopics',[TeacherController::class,'viewTopics'])->name('teacher.viewTopics');
+
+Route::post('/teacher/save',[TeacherController::class, 'save'])->name('teacher.save');
+Route::post('/teacher/check',[TeacherController::class,'check'])->name('teacher.check');
+Route::get('/teacher/logout',[TeacherController::class,'logout'])->name('teacher.logout');
+
+
+
+
+Route::group(['middleware'=>['TeacherCheck']], function(){
+    Route::get('/teacher/login',[TeacherController::class, 'login'])->name('teacher.login');
+    Route::get('/teacher/register',[TeacherController::class, 'register'])->name('teacher.register');
+
+    
+    Route::get('/teacher/dashboard',[TeacherController::class,'dashboard'])->name('teacher.dashboard');   
+});
